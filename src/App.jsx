eec1c7ad4f291738eef075bae2f6fdea503da6a7
ricardo <nocive.science@gmail.com>
2024-04-09 03:33:59 -0400

    primer commit al master

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import NavBar from './components/NavBar'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+    </>
+  )
+}
+
+export default App
